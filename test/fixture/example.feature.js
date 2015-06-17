8a68bb257d102b530e74b40c8025bfb926ff1591
John Wakley <johnwakley@gmail.com>
2015-06-17 23:20:26 +0100

    Add fixture data

---
+++ test/fixture/example.feature.js
@@ -0,0 +1,33 @@
+const feature = `
+@feature-tag
+Feature: Sample Feature
+  In order to <impact>
+  As an <actor>
+  I want to <what>
+
+  Background:
+    Given some background
+
+  @foo @bar
+  Scenario: Scenario One
+  This is a scenario description
+    Given context
+    When action
+    Then result
+    """
+    This is a doc string
+    """
+
+  @baz
+  Scenario Outline: Scenario Two
+    Given <one>
+    When <two>
+    Then <three>
+
+    Examples:
+      | one | two  | three |
+      | un  | deux | trois |
+      | uno | dos  | tres  |
+`;
+
+module.exports = feature;
--- test/gherkinParserTest.js
+++ test/gherkinParserTest.js
@@ -5,24 +5,25 @@ const sinon = require("sinon");
 // Lib
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
-
-//const Gherkin = require.main.require('lib/gherkin/gherkin');
-const Gherkin = require.main.require('gherkin');
+const Gherkin = require('gherkin');
 
 // Core
 const Path = require("path");
+
+// Fixtures
+const spec = require.main.require('test/fixture/example.feature.js');
 
 // App modules
 
 
 describe('Parse specification', function() {
-    it('should parse a correctly formatted specification', () => {
+    it.only('should parse a correctly formatted specification', () => {
         const parser = new Gherkin.Parser();
 
         const builder = new Gherkin.AstBuilder();
         parser.stopAtFirstError = false;
 
-        const scanner = new Gherkin.TokenScanner('Feature: Parse specification');
+        const scanner = new Gherkin.TokenScanner(spec);
         const ast = parser.parse(scanner, builder, new Gherkin.TokenMatcher());
         const result = JSON.stringify(ast, null, 2);
 
